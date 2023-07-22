047648ca07c29e74bb0762f00957714d5f75c62f
Adenola Omotayo <102471748+Beenflexxin01@users.noreply.github.com>
2023-07-22 16:37:10 +0100

    add

--- app.js
+++ app.js
@@ -32,13 +32,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 // GLOBAL MIDDLEWARES
 // Set security HTTP headers
-app.use(helmet.crossOriginEmbedderPolicy({ policy: 'credentialless' }));
 
 app.use(cors());
 // Access-Control-Allow-Origin: *
 
 // Allow all options
 app.options('*', cors());
+
+app.use(helmet.crossOriginEmbedderPolicy({ policy: 'credentialless' }));
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
